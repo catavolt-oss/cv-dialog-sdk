addc97ca8ca18a76dd2d089048d9bf5eb54fa89b
Rob Burson <rburson@gmail.com>
2015-12-09 12:33:49 -0500

    added method to workbench

--- src/catavolt/dialog/Workbench.ts
+++ src/catavolt/dialog/Workbench.ts
@@ -14,6 +14,16 @@ module catavolt.dialog {
                     private _actions:Array<WorkbenchLaunchAction>) {}
 
         get alias() { return this._alias; }
+        getLaunchActionById(launchActionId:string) {
+                var result = null;
+                this.workbenchLaunchActions.some(function(launchAction){
+                    if(launchAction.id = launchActionId) {
+                        result = launchAction;
+                        return true;
+                    }
+                });
+                return result;
+        }
         get name() { return this._name; }
         get workbenchId() { return this._id; }
         get workbenchLaunchActions() { return ArrayUtil.copy(this._actions); }
