7172cf913ad479f7c02780b0c6ca32010eeb2959
Rob Burson <rburson@gmail.com>
2017-04-12 09:01:11 -0400

    removed js and d.ts

--- src/js/catavolt.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by rburson on 1/27/16.
- */
-export * from './catavolt/util';
-export * from './catavolt/fp';
-export * from './catavolt/ws';
-export * from './catavolt/dialog';
-export * from './catavolt/print';
