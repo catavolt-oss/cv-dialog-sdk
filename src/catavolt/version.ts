d1a89ee561af6a3d62591dd3f4e0c39db4021bb4
Rob Burson <rburson@gmail.com>
2017-06-21 10:43:23 -0400

    commit 1.1.27

--- src/catavolt/version.ts
+++ src/catavolt/version.ts
@@ -1,7 +1,7 @@
 import {AppContext} from "./dialog"
 import * as moment from 'moment-timezone';
 
-export const CATAVOLT_SDK_VERSION:string = '1.1.26';
+export const CATAVOLT_SDK_VERSION:string = '1.1.27';
 
 (()=>{
     AppContext.singleton.addStaticDeviceProp('catavoltSdkVersion:' + CATAVOLT_SDK_VERSION);
