29ec517209aa2e4b049cca4f9c8df5df2e455a3e
Rob Burson <rburson@gmail.com>
2017-06-27 15:50:13 -0400

    commit 1.1.29

--- src/catavolt/version.ts
+++ src/catavolt/version.ts
@@ -1,10 +1,11 @@
 import {AppContext} from "./dialog"
 import * as moment from 'moment-timezone';
 
-export const CATAVOLT_SDK_VERSION:string = '1.1.28';
+export const CATAVOLT_SDK_VERSION:string = '1.1.29';
 
 (()=>{
+    AppContext.singleton.addDynamicDeviceProp(():string=>{ return 'platform:browser'});
     AppContext.singleton.addStaticDeviceProp('catavoltSdkVersion:' + CATAVOLT_SDK_VERSION);
-    AppContext.singleton.addDynamicDeviceProp(():string=>{ return 'DeviceTime:' +  (new Date()).toTimeString()});
-    AppContext.singleton.addDynamicDeviceProp(():string=>{ return 'DeviceTimeZone:' +  moment.tz.guess()});
+    AppContext.singleton.addDynamicDeviceProp(():string=>{ return 'deviceTime:' +  (new Date()).toISOString()});
+    AppContext.singleton.addDynamicDeviceProp(():string=>{ return 'deviceTimeZone:' +  moment.tz.guess()});
 })();
