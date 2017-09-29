983713b58b7fc9b5351ddfb631bdfe57cfd3ac27
Rob Burson <rburson@gmail.com>
2017-09-29 13:50:18 -0400

    commit 1.1.39

--- src/catavolt/version.ts
+++ src/catavolt/version.ts
@@ -1,7 +1,7 @@
 import {AppContext} from "./dialog"
 import * as moment from 'moment-timezone';
 
-export const CATAVOLT_SDK_VERSION:string = '1.1.38';
+export const CATAVOLT_SDK_VERSION:string = '1.1.39';
 
 (()=>{
     AppContext.singleton.addDynamicDeviceProp(():string=>{ return 'platform:browser'});
