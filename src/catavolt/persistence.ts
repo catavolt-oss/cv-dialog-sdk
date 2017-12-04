8634d90ac1d14b4f10a347a2d515e16cd4381d67
glennosborne <glenn.osborne@catavolt.com>
2017-12-04 09:16:35 -0500

    Changed PersistentClient to call-through to FetchClient

--- src/catavolt/persistence.ts
+++ src/catavolt/persistence.ts
@@ -2,34 +2,40 @@
  * Created by rburson on 10/16/17.
  */
 
-import {StringDictionary, Log} from "./util";
-import {Client, JsonClientResponse, TextClientResponse, VoidClientResponse, ClientMode} from "./client"
+import {Log, StringDictionary} from "./util";
+import {Client, ClientMode, JsonClientResponse, TextClientResponse, VoidClientResponse} from "./client"
+import {FetchClient} from "./ws";
 
-export class PersistentClient implements Client{
+export class PersistentClient implements Client {
 
     /* Last operation happened at this time */
     private _lastActivity: Date = new Date();
-    private _clientMode:ClientMode;
+    private _clientMode: ClientMode;
+    private _fetchClient: FetchClient;
 
-    constructor(clientMode:ClientMode = ClientMode.REMOTE) {
+    constructor(clientMode: ClientMode = ClientMode.REMOTE) {
         this._clientMode = clientMode;
+        this._fetchClient = new FetchClient();
     }
 
     get(baseUrl: string, resourcePath?: string): Promise<TextClientResponse> {
-        let response:Promise<TextClientResponse> = null;
-        const url = resourcePath ? `${baseUrl}/${resourcePath}` : baseUrl;
+        Log.debug("BEGIN GETTING");
+        let response: Promise<TextClientResponse> = this._fetchClient.get(baseUrl, resourcePath);
+        Log.debug("END GETTING");
         return response;
     }
 
     postMultipart(baseUrl: string, resourcePath: string, formData: FormData): Promise<VoidClientResponse> {
-        let response:Promise<VoidClientResponse> = null;
-        const url = resourcePath ? `${baseUrl}/${resourcePath}` : baseUrl;
+        Log.debug("BEGIN POST MULTIPART");
+        let response: Promise<VoidClientResponse> = this._fetchClient.postMultipart(baseUrl, resourcePath, formData);
+        Log.debug("END POST MULTIPART");
         return response;
     }
 
-    getJson(baseUrl: string, resourcePath: string, queryParams:StringDictionary): Promise<JsonClientResponse> {
-        let response:Promise<JsonClientResponse> = null;
-        const url = resourcePath ? `${baseUrl}/${resourcePath}` : baseUrl;
+    getJson(baseUrl: string, resourcePath?: string, queryParams?: StringDictionary): Promise<JsonClientResponse> {
+        Log.debug("BEGIN GET JSON");
+        let response: Promise<JsonClientResponse> = this._fetchClient.getJson(baseUrl, resourcePath, queryParams);
+        Log.debug("END GET JSON");
         return response;
     }
 
@@ -38,25 +44,30 @@ export class PersistentClient implements Client{
     }
 
     postJson(baseUrl: string, resourcePath: string, jsonBody?: StringDictionary): Promise<JsonClientResponse> {
-        let response:Promise<JsonClientResponse> = null;
-        const url = resourcePath ? `${baseUrl}/${resourcePath}` : baseUrl;
+        Log.debug("BEGIN POST JSON");
+        let response: Promise<JsonClientResponse> = this._fetchClient.postJson(baseUrl, resourcePath, jsonBody);
+        Log.debug("END POST JSON");
         return response;
     }
 
     putJson(baseUrl: string, resourcePath: string, jsonBody?: StringDictionary): Promise<JsonClientResponse> {
-        let response:Promise<JsonClientResponse> = null;
-        const url = resourcePath ? `${baseUrl}/${resourcePath}` : baseUrl;
+        Log.debug("BEGIN PUT JSON");
+        let response: Promise<JsonClientResponse> = this._fetchClient.putJson(baseUrl, resourcePath, jsonBody);
+        Log.debug("END PUT JSON");
         return response;
     }
 
     deleteJson(baseUrl: string, resourcePath: string): Promise<JsonClientResponse> {
-        let response:Promise<JsonClientResponse> = null;
-        const url = resourcePath ? `${baseUrl}/${resourcePath}` : baseUrl;
+        Log.debug("BEGIN DELETE JSON");
+        let response: Promise<JsonClientResponse> = this._fetchClient.deleteJson(baseUrl, resourcePath);
+        Log.debug("END DELETE JSON");
         return response;
     }
 
-    setClientMode(clientMode:ClientMode):void {
+    setClientMode(clientMode: ClientMode): void {
+        Log.debug("BEGIN SET CLIENT MODE");
         this._clientMode = clientMode;
+        Log.debug("END SET CLIENT MODE");
     }
 
 }
