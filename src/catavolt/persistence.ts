18d9d8da0deaaa223484190a85cf8778a6e2d681
glennosborne <glenn.osborne@catavolt.com>
2017-12-17 10:31:14 -0500

    Added service worker module

--- src/catavolt/persistence.ts
+++ src/catavolt/persistence.ts
@@ -1,73 +1,103 @@
 /**
- * Created by rburson on 10/16/17.
+State * Created by rburson on 10/16/17.
  */
 
-import {Log, StringDictionary} from "./util";
+import {StringDictionary} from "./util";
 import {Client, ClientMode, JsonClientResponse, TextClientResponse, VoidClientResponse} from "./client"
 import {FetchClient} from "./ws";
+import {PersistenceTools} from "./persistence-tools";
 
 export class PersistentClient implements Client {
 
-    /* Last operation happened at this time */
-    private _lastActivity: Date = new Date();
     private _clientMode: ClientMode;
     private _fetchClient: FetchClient;
+    private _tenantId: string;
+    private _userId: string;
 
-    constructor(clientMode:ClientMode = ClientMode.ONLINE) {
+    constructor(clientMode: ClientMode = ClientMode.ONLINE) {
         this._clientMode = clientMode;
         this._fetchClient = new FetchClient();
+        this._tenantId = undefined;
+        this._userId = undefined;
     }
 
-    get(baseUrl: string, resourcePath?: string): Promise<TextClientResponse> {
-        Log.debug("BEGIN GETTING");
-        let response: Promise<TextClientResponse> = this._fetchClient.get(baseUrl, resourcePath);
-        Log.debug("END GETTING");
-        return response;
-    }
-
-    postMultipart(baseUrl: string, resourcePath: string, formData: FormData): Promise<VoidClientResponse> {
-        Log.debug("BEGIN POST MULTIPART");
-        let response: Promise<VoidClientResponse> = this._fetchClient.postMultipart(baseUrl, resourcePath, formData);
-        Log.debug("END POST MULTIPART");
-        return response;
-    }
-
-    getJson(baseUrl: string, resourcePath?: string, queryParams?: StringDictionary): Promise<JsonClientResponse> {
-        Log.debug("BEGIN GET JSON");
-        let response: Promise<JsonClientResponse> = this._fetchClient.getJson(baseUrl, resourcePath, queryParams);
-        Log.debug("END GET JSON");
-        return response;
-    }
+    /* Last operation happened at this time */
+    private _lastActivity: Date = new Date();
 
     get lastActivity(): Date {
         return this._lastActivity;
     }
 
+    get(baseUrl: string, resourcePath?: string): Promise<TextClientResponse> {
+        let response: Promise<TextClientResponse> = this._fetchClient.get(baseUrl, resourcePath);
+        return response;
+    }
+
+    postMultipart(baseUrl: string, resourcePath: string, formData: FormData): Promise<VoidClientResponse> {
+        let response: Promise<VoidClientResponse> = this._fetchClient.postMultipart(baseUrl, resourcePath, formData);
+        return response;
+    }
+
+    getJson(baseUrl: string, resourcePath?: string, queryParams?: StringDictionary): Promise<JsonClientResponse> {
+        let response: Promise<JsonClientResponse> = this._fetchClient.getJson(baseUrl, resourcePath, queryParams);
+        const path: string[] = resourcePath.split('/');
+        if (PersistenceTools.isGetDialog(path)) {
+            response.then(jcr => {
+                if (jcr.statusCode == 200) {
+                    const dialog = <StringDictionary> jcr.value;
+                    PersistenceTools.writeDialogState(this._tenantId, this._userId, dialog);
+                }
+            });
+        }
+        return response;
+    }
+
     postJson(baseUrl: string, resourcePath: string, jsonBody?: StringDictionary): Promise<JsonClientResponse> {
-        Log.debug("BEGIN POST JSON");
         let response: Promise<JsonClientResponse> = this._fetchClient.postJson(baseUrl, resourcePath, jsonBody);
-        Log.debug("END POST JSON");
+        const path: string[] = resourcePath.split('/');
+        if (PersistenceTools.isPostSession(path)) {
+            response.then(jcr => {
+                if (jcr.statusCode == 200) {
+                    const session = <StringDictionary> jcr.value;
+                    this._tenantId = session.tenantId;
+                    this._userId = session.userId;
+                    PersistenceTools.deleteAllState(this._tenantId, this._userId);
+                    PersistenceTools.writeSessionState(session);
+                } else if (jcr.statusCode == 303) {
+                    const redirection = <StringDictionary> jcr.value;
+                    PersistenceTools.writeRedirectionState(this._tenantId, this._userId, redirection);
+                }
+            });
+        } else if (PersistenceTools.isPostMenuAction(path)) {
+            response.then(jcr => {
+                if (jcr.statusCode == 303) {
+                    const redirection = <StringDictionary> jcr.value;
+                    PersistenceTools.writeRedirectionState(this._tenantId, this._userId, redirection);
+                }
+            });
+        } else if (PersistenceTools.isPostWorkbenchAction(path)) {
+            response.then(jcr => {
+                if (jcr.statusCode == 303) {
+                    const redirection = <StringDictionary> jcr.value;
+                    PersistenceTools.writeRedirectionState(this._tenantId, this._userId, redirection);
+                }
+            });
+        }
         return response;
     }
 
     putJson(baseUrl: string, resourcePath: string, jsonBody?: StringDictionary): Promise<JsonClientResponse> {
-        Log.debug("BEGIN PUT JSON");
         let response: Promise<JsonClientResponse> = this._fetchClient.putJson(baseUrl, resourcePath, jsonBody);
-        Log.debug("END PUT JSON");
         return response;
     }
 
     deleteJson(baseUrl: string, resourcePath: string): Promise<JsonClientResponse> {
-        Log.debug("BEGIN DELETE JSON");
         let response: Promise<JsonClientResponse> = this._fetchClient.deleteJson(baseUrl, resourcePath);
-        Log.debug("END DELETE JSON");
         return response;
     }
 
     setClientMode(clientMode: ClientMode): void {
-        Log.debug("BEGIN SET CLIENT MODE");
         this._clientMode = clientMode;
-        Log.debug("END SET CLIENT MODE");
     }
 
 }
