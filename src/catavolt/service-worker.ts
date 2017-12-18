b30fdf863a82d5eef494f339c437cb26ea1af889
glennosborne <glenn.osborne@catavolt.com>
2017-12-17 20:43:51 -0500

    Added "cache-all" behavior to service-worker.ts

--- src/catavolt/service-worker.ts
+++ src/catavolt/service-worker.ts
@@ -2,30 +2,53 @@
 import {PersistenceTools} from "./persistence-tools";
 
 const MessagePrefix = `[Catavolt ServiceWorker ${(new Date()).toLocaleString()}]`;
+const CacheName = 'catavolt.tenants';
 
-if (typeof self !== 'undefined' && (typeof window === 'undefined' || self !== window)) {
-    console.log(`${MessagePrefix} loading at: ${self}`);
-    console.log(`${MessagePrefix} adding "install" listener`);
-    self.addEventListener('install', event => {
-        ServiceWorker.install();
-    });
-    console.log(`${MessagePrefix} adding "fetch" listener`);
-    self.addEventListener('fetch', event => {
-        ServiceWorker.fetch(event);
-    });
-    console.log(`${MessagePrefix} loading completed`);
-} else {
-    console.log(`${MessagePrefix} cannot load at: ${self}`);
-}
+console.log(`${MessagePrefix} loading at: ${self}`);
+console.log(`${MessagePrefix} adding "install" listener`);
+self.addEventListener('install', event => {
+    ServiceWorker.install(event);
+});
+console.log(`${MessagePrefix} adding "fetch" listener`);
+self.addEventListener('fetch', event => {
+    ServiceWorker.fetch(event);
+});
+console.log(`${MessagePrefix} loading completed`);
 
 export class ServiceWorker {
 
-    public static install() {
-        console.log(`${MessagePrefix} install() completed successfully`)
+    public static install(event) {
+        event.waitUntil(() => {
+            caches.open(CacheName).then(cache => cache.addAll([
+                'bundle.js',
+                'application.png'
+            ]));
+        });
+        console.log(`${MessagePrefix} install() completed`);
     }
 
     public static fetch(event) {
         console.log(`${MessagePrefix} fetch() intercepted request: ${event.request.url}`);
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    console.log(`${MessagePrefix} fetch() reponse found in cache: ${event.request.url}`);
+                    return cachedResponse;
+                }
+                var requestToCache = event.request.clone();
+                return fetch(requestToCache).then(fetchResponse => {
+                    if (!fetchResponse || fetchResponse.status !== 200) {
+                        return fetchResponse;
+                    }
+                    var responseToCache = fetchResponse.clone();
+                    caches.open(CacheName).then(cache => {
+                        console.log(`${MessagePrefix} fetch() successfully cached response from: ${requestToCache.url}`);
+                        cache.put(requestToCache, responseToCache);
+                    });
+                    return fetchResponse;
+                });
+            })
+        );
     }
 
 }
