bb24e36ed2c504b8d87bfaf5c24b10f78c45f76d
Rob Burson <rburson@gmail.com>
2018-08-28 14:54:50 -0400

    chore: remove set property to 'browser'

--- src/catavolt/version.ts
+++ src/catavolt/version.ts
@@ -5,7 +5,6 @@ export const CATAVOLT_SDK_VERSION:string = '4.25.0';
 
 (()=>{
     Catavolt.addStaticDeviceProp('catavoltSdkVersion', CATAVOLT_SDK_VERSION);
-    Catavolt.addDynamicDeviceProp('platform', ()=>'browser');
     Catavolt.addDynamicDeviceProp('deviceTime', ()=>moment(new Date()).format());
     Catavolt.addDynamicDeviceProp('deviceTimeZone', ()=>moment.tz.guess());
 })();
